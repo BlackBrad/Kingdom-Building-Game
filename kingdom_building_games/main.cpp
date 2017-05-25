087cf1f26adadbe82fc58a4fe1519509649bf6b7
BlackBrad <charliefan39@gmail.com>
2017-05-25 15:41:04 +1200

    Initial Commit

---
+++ kingdom_building_games/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kingdom_building_games
+//
+//  Created by Bradley Black on 5/25/17.
+//  Copyright © 2017 Bradley Black. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
